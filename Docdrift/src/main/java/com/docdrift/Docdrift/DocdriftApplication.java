4a3f44c17e6134d8964df30c1e8497c1d0d2d42c
Karthig Rajan S <rajankarthig@gmail.com>
2026-07-25 09:57:16 +0530

    Intial commit

---
+++ Docdrift/src/main/java/com/docdrift/Docdrift/DocdriftApplication.java
@@ -0,0 +1,13 @@
+package com.docdrift.Docdrift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocdriftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocdriftApplication.class, args);
+	}
+
+}
---
+++ Docdrift/src/test/java/com/docdrift/Docdrift/DocdriftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docdrift.Docdrift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocdriftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
